cc00ac360ec1c10f7780472af20274ea77640791
Jacob Johansson <jacobknuts@gmail.com>
2026-03-25 22:26:05 +0100

    feat: Add navigation links for experience, education, languages, and interests.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,8 +18,12 @@ const Navigation = () => {
         </a>
         <ul className="nav-links">
           <li><a href="#about" onClick={(e) => scrollTo(e, 'about')}>Om mig</a></li>
+          <li><a href="#experience" onClick={(e) => scrollTo(e, 'experience')}>Erfarenhet</a></li>
+          <li><a href="#education" onClick={(e) => scrollTo(e, 'education')}>Utbildning</a></li>
           <li><a href="#skills" onClick={(e) => scrollTo(e, 'skills')}>Kunskaper</a></li>
           <li><a href="#projects" onClick={(e) => scrollTo(e, 'projects')}>Projekt</a></li>
+          <li><a href="#languages" onClick={(e) => scrollTo(e, 'languages')}>Språk</a></li>
+          <li><a href="#interests" onClick={(e) => scrollTo(e, 'interests')}>Intressen</a></li>
           <li><a href="#contact" onClick={(e) => scrollTo(e, 'contact')}>Kontakt</a></li>
         </ul>
       </div>
